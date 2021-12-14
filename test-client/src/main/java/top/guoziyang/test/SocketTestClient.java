3f023cb9ed1a0cf8da9370860b1cc1c0fab79c75
blackprm <1144569608@qq.com>
2021-12-14 22:13:37 +0800

    commit

--- test-client/src/main/java/top/guoziyang/test/SocketTestClient.java
+++ test-client/src/main/java/top/guoziyang/test/SocketTestClient.java
@@ -15,14 +15,16 @@ import top.guoziyang.rpc.transport.socket.client.SocketClient;
 public class SocketTestClient {
 
     public static void main(String[] args) {
-        SocketClient client = new SocketClient(CommonSerializer.KRYO_SERIALIZER);
-        RpcClientProxy proxy = new RpcClientProxy(client);
-        HelloService helloService = proxy.getProxy(HelloService.class);
-        HelloObject object = new HelloObject(12, "This is a message");
-        String res = helloService.hello(object);
-        System.out.println(res);
-        ByeService byeService = proxy.getProxy(ByeService.class);
-        System.out.println(byeService.bye("Netty"));
+        for(int i = 0;i < 1000;i++) {
+            SocketClient client = new SocketClient(CommonSerializer.KRYO_SERIALIZER);
+            RpcClientProxy proxy = new RpcClientProxy(client);
+            HelloService helloService = proxy.getProxy(HelloService.class);
+            HelloObject object = new HelloObject(12, "This is a message");
+            String res = helloService.hello(object);
+            System.out.println(res);
+            ByeService byeService = proxy.getProxy(ByeService.class);
+            System.out.println(byeService.bye("Netty"));
+        }
     }
 
 }
